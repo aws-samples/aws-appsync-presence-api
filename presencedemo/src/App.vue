ff82c4092417f485bec4219a2e10c5db8073031c
Olivier Chappe <chappo@amazon.com>
2020-12-09 18:35:11 +0100

    Removed logo and updated screenshots

--- presencedemo/src/App.vue
+++ presencedemo/src/App.vue
@@ -2,11 +2,6 @@
   <div>
     <h1>Presence API Demo</h1>
     <player-list></player-list>
-    <div class="footer">
-      <a hrel="https://aws.amazon.com/gametech">
-        <img class="logo" alt="AWS Game Tech logo" src="./assets/game_tech.png" />
-      </a>
-    </div>
   </div>
 </template>
 
